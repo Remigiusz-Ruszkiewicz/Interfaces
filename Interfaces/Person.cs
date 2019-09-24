9a561d8fc673772999578bdcfd3ebf87bab93ef1
Remigiusz Ruszkiewicz <r.ruszkiewicz@microsoft.wsei.edu.pl>
2019-09-24 18:13:04 +0200

    Add project files.

---
+++ Interfaces/Person.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Win32.SafeHandles;
+using System;
+using System.Runtime.InteropServices;
+
+namespace Interfaces
+{
+    public class Person : IDisposable
+    {
+        public string Name { get; set; }
+
+
+        bool disposed = false;
+
+        SafeHandle handle = new SafeFileHandle(IntPtr.Zero, true);
+
+        public void Dispose()
+
+        {
+
+            Dispose(true);
+
+            GC.SuppressFinalize(this);
+
+        }
+
+
+
+        protected virtual void Dispose(bool disposing)
+
+        {
+
+            if (disposed)
+
+                return;
+
+            if (disposing)
+
+            {
+
+                handle.Dispose();
+
+            }
+
+            disposed = true;
+
+        }
+    }
+}
---
+++ Interfaces/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    class Program
+    {
+        const string x = "X";
+        readonly string y;
+        static void Main(string[] args)
+        {
+            //List<Person> names = new List<Person>
+            //{
+            //    new Person{Name = "A" },
+            //    new Person{Name = "AA" },
+            //    new Person{Name = "ABA" },
+            //    new Person{Name = "AAAA" },
+            //    new Person{Name = "ACBA" },
+            //};
+            //foreach (var item in names)
+            //{
+            //    item.Name = "S";
+            //    Console.WriteLine(item.Name);
+            //}
+            // wer1
+            Person person = new Person();
+            try
+            {
+                Console.WriteLine(person.Name);
+            }
+            finally
+            {
+                person.Dispose();
+            }
+            // wer2
+            using (Person person2 = new Person())
+            {
+                Console.WriteLine(person.Name);
+            }
+            Console.Read();
+        }
+    }
+}
